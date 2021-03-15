ANALYSIS - added options for reading various CMRR sLASER twix data sets and setting scales correct. Added new nomeclature for LMFIT. Working towards adding the Jacobian to the call.

vespa/analysis/chain_fit_voigt.py
@@ -283,6 +283,31 @@ class ChainFitVoigt(Chain):
                         
         return plot_results
 
+    def lorgauss_internal_lmfit_dfunc(self, params):
+        """
+        This is in the format that LMFIT expects to call in the Minimizer class.
+
+        This returns the weighted difference (data - yfit) * ww as a single
+        numpy float array, where the real and imaginary vectors have been
+        concatenated into a single array.
+
+        """
+        keep = []
+        for i, key in enumerate(params.keys()):
+            if params[key].expr is None:
+                keep.append(i)
+
+        yfit, all_pders = self.lorgauss_internal(params, pderflg=True)
+
+        pders = all_pders[keep,:]
+
+        dfunc = []
+        for pder in pders:
+            dfunc.append(np.concatenate([pder.real, pder.imag]))
+        dfunc = np.array(dfunc)
+
+        return dfunc
+
 
     def lorgauss_internal_lmfit(self, params, report_stats=False):
         """
@@ -296,7 +321,7 @@ class ChainFitVoigt(Chain):
         data  = self.data_scale.copy()
         ww    = self.weight_array
                 
-        yfit, pder = self.lorgauss_internal(params)
+        yfit, pder = self.lorgauss_internal(params, pderflg=False)
         
         yfit = np.concatenate([yfit.real, yfit.imag])
         data = np.concatenate([data.real, data.imag])

vespa/analysis/fileio/siemens_twix_slaser_cmrr.py
@@ -127,11 +127,6 @@ class RawReaderSiemensTwixSlaserCmrrVbGulinLong(RawReaderSiemensTwixSlaserCmrrVe
 
         _, _, navg, npts = data.shape
 
-        # gives integral values of 1 or larger for nice display
-        scale = RAWDATA_SCALE / float(navg)
-        data = data * scale
-        prep = prep * scale
-
         if d["remove_os"]:
             data = self._remove_oversampling_basic(data)
             if prep is not None:
@@ -152,7 +147,7 @@ class RawReaderSiemensTwixSlaserCmrrVbGulinLong(RawReaderSiemensTwixSlaserCmrrVe
 
         # dataset2 - scan 1-2, water unsuppressed for water scale
 
-        d["data"] = prep[:,:,1:2,:].copy() * RAWDATA_SCALE / float(2.0)
+        d["data"] = prep[:,:,1:2,:].copy() * RAWDATA_SCALE / float(1.0)
         d["data_source"] = filename + '.water1'
         raws.append(DataRawCmrrSlaser(d))
 
@@ -165,4 +160,57 @@ class RawReaderSiemensTwixSlaserCmrrVbGulinLong(RawReaderSiemensTwixSlaserCmrrVe
         return raws
 
 
+class RawReaderSiemensTwixSlaserCmrrVbGulinLongWater(RawReaderSiemensTwixSlaserCmrrVe):
+
+    def read_raw(self, filename, ignore_data=False, open_dataset=None):
+        """
+        Given filename, returns a DataRawCmrrSlaser object.
+        - The ignore_data option is not implemented for this reader.
+        - The open_dataset attribute is not used in this reader.
+
+        """
+        twix, version_flag = self.get_twix(filename)
+
+        # 'scan' returns data in [ncha, navg, npts] order - easy to convert to mrs_data_raw dims
+        d = self._get_parameters(twix.current, index='scan')
+        d["data_source"] = filename
+
+        data = d['data']
+        prep = d['prep']
+        nrep, _, navg, npts = data.shape
+
+        if d["remove_os"]:
+            data = self._remove_oversampling_basic(data)
+            if prep is not None:
+                prep = self._remove_oversampling_basic(prep)
+            d["sw"] = d["sw"] / 2.0
+
+        data = np.conjugate(data)
+        if prep is not None:
+            prep = np.conjugate(prep)
+
+        raws = []
+
+        # dataset1 - scan 0, water unsuppressed for coil combine and (maybe) ECC
+
+        d["data"] = prep[:,:,0,:].copy() * RAWDATA_SCALE / float(1.0)
+        d["data_source"] = filename + '.combine'
+        raws.append(DataRawCmrrSlaser(d))
+
+        # dataset2 - scan 1-2, water unsuppressed for water scale
+
+        if prep.shape[2] > 1:
+            d["data"] = prep[:,:,1,:].copy() * RAWDATA_SCALE / float(1.0)
+        else:
+            d["data"] = prep[:,:,0,:].copy() * RAWDATA_SCALE / float(1.0)
+        d["data_source"] = filename + '.water1'
+        raws.append(DataRawCmrrSlaser(d))
+
+        # dataset3 - scans 5-68 (64 total), metabolite data with WS
+
+        d["data"] = data.copy() * RAWDATA_SCALE / float(navg)
+        d["data_source"] = filename + '.metab64'
+        raws.append(DataRawCmrrSlaser(d))
+
+        return raws
 

vespa/common/twix_parser.py
@@ -677,6 +677,62 @@ class TwixRaid(object):
 
         return nparr
 
+
+    def get_data_numpy_channel_scan_order_with_prep(self):
+        """
+        Sort all scans into numpy array in [fid, channel_id, spectral points] order.
+
+        This does not require unique dimensions. It does require that total
+        number of scans divided by number of channels is an integer. It is
+        assumed that all channels for a given FID follow one after the other
+        in the scan list.
+
+        """
+        npts = self.scans[0].samples_in_scan
+        nscan = len(self.scans)
+        ncha = self.scans[0].used_channels
+
+        nrep = self.dims[SCAN_INDICES.index('repetition')]  # number of repetitions, may be 1
+        nset = self.dims[SCAN_INDICES.index('set')]  # number of FIDs for spectroscopy
+        ncha2 = self.dims[SCAN_INDICES.index('channel_id')]  # number of channels, may be 1
+
+
+        if (nscan % ncha) > 0:
+            msg = "Number of scans not divisible by number of channels, inconsistent array size, returning.."
+            raise ValueError(msg)
+
+        nfid = int(nscan / ncha)
+
+        header = _parse_protocol_data(self.evps[3][1])
+        nprep_hdr = int(header['sSpecPara.lPreparingScans']) if 'sSpecPara.lPreparingScans' in header.keys() else 0
+        nprep = nfid - nrep * nset
+        if nprep_hdr != nprep: print('Warning: get_data_numpy_scan_channel_order_with_prep() - nprep_hdr != nprep')
+
+        # I have seen in CMRR sLASER data that the scan.set value does not increment
+        # while the prep scans are taken. We get around this empirically. We know
+        # how many Averages are being taken, multiply by NCha and subtract from the
+        # total number of scans and divide by NCha and we know how many prep averages
+        # there will be.  I calc the iset index below using this sort of math. Just
+        # in case someday it start to increment, I make allowances for that case too.
+
+        prep_arr = None
+        if nprep != 0:
+            prep_arr = np.zeros([ncha, nprep, npts], np.complex)
+            for iscan, scan in enumerate(self.scans[0:int(nprep * ncha)]):
+                ifid = iscan // ncha
+                icha = scan.channel_id
+                prep_arr[icha, ifid, :] = np.array(scan.data)
+
+        nparr = np.zeros([ncha, nfid-nprep, npts], np.complex)
+
+        for iscan, scan in enumerate(self.scans[int(nprep * ncha):]):
+            ifid = iscan // ncha
+            icha = scan.channel_id
+            nparr[icha, ifid, :] = np.array(scan.data)
+
+        return nparr, prep_arr
+
+
     def get_data_numpy_rep_coil_avg_npts_order(self, return_prep=False):
         return self.get_data_numpy_rep_channel_set_order(return_prep=return_prep)
 
@@ -714,7 +770,7 @@ class TwixRaid(object):
             # I have seen in CMRR sLASER data that the scan.set value does not increment
             # while the prep scans are taken. We get around this empirically. We know
             # how many Averages are being taken, multiply by NCha and subtract from the
-            # total number of scans and divid by NCha and we know how many prep averages
+            # total number of scans and divide by NCha and we know how many prep averages
             # there will be.  I calc the iset index below using this sort of math. Just
             # in case someday it start to increment, I make allowances for that case too.
 