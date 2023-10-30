093688c6fae6f78ded0f501d678fee6e26f8e8bc
Brian J Soher <bsoher@briansoher.com>
2023-10-30 18:01:29 -0400

    PULSE - refactor algorithm (1st half) for refocus gradient.  Just to better understand algo, and more pythonic

--- vespa/common/rfp_rf_result.py
+++ vespa/common/rfp_rf_result.py
@@ -162,6 +162,8 @@ class RfResults(object):
 
         try:
             val = self.grad_refocus(ax, profile, pulse_dur_ms)
+            val2 = self.grad_refocus_orig(ax, profile, pulse_dur_ms)
+            print('orig val = '+str(val2)+'  new val = '+str(val))
             val = val / scale
         except PulseFuncException:
             val = 0.5
@@ -228,6 +230,20 @@ class RfResults(object):
         pulse_dur_ms - The pulse length in milliseconds.
         pulse_bw_khz - The pulse bandwidth in khz.
 
+        BJS - just FYI, after beating my head against this, here's my wisdom ...
+         The value that comes out of this is the factor that multiplies the
+         waveform to minimize the area of the imag, and max the area of the
+         real plot(s). And that is based on the 'standard' gradient value that
+         is typically set (10mT/m, 1g/cm) if no gradient is calculated for the
+         waveform algorithm.  So if I get 0.515, that means that I have to
+         apply 51.5% of the area of the gradient to refocust the signal. In
+         Siemens-world, the PulseAsymmetry() is set as the % from the START
+         of the waveform to the 'phase-center' of the pulse. So I would have
+         to set that to 1-0.515 (in this example) to properly set their pulse
+         attribute.
+
+        Oct 30, 2023 - bjs first pass refactor, mainly 1st half the code, not the actual algorithm
+
         """
         # Get the bandwidth from the profile
         pulse_bw_khz = self.get_bandwidth(xaxis0, yprofile0)
@@ -240,42 +256,19 @@ class RfResults(object):
         yprofile = yprofile0.copy()
         bw_half = pulse_bw_khz/2.0
 
-        found_ax1 = False 
-        found_ax2 = False
-        index = 0
-        for ii in xaxis:
-            if ii > -bw_half and not found_ax1:
-                # Want the first one, so if find one, don't reassign.
-                ax1 = index
-                found_ax1 = True
-            if ii < bw_half:
-                # Keep reassigning, since want last one.
-                ax2 = index
-                found_ax2 = True
-            index += 1
+        ax1  = next(x[0] for x in enumerate(xaxis) if x[1] > -bw_half)
+        ax2  = len(xaxis) - next(x[0] for x in enumerate(xaxis[::-1]) if x[1] < bw_half) - 1
+        ax11 = next(x[0] for x in enumerate(xaxis) if x[1] > -2*bw_half)
+        ax22 = len(xaxis) - next(x[0] for x in enumerate(xaxis[::-1]) if x[1] < 2*bw_half) - 1
 
-        found_ax11 = False 
-        found_ax22 = False
-        index = 0
-        for ii in xaxis:
-            if ii > -2*bw_half and not found_ax11:
-                # Want the first one, so if find one, don't reassign.
-                ax11 = index
-                found_ax11 = True
-            if ii < 2*bw_half:
-                # Keep reassigning, since want last one.
-                ax22 = index
-                found_ax22 = True
-            index += 1
-
-        # If not (found_ax1 and found_ax2), throw an error.
-        if not (found_ax1 and found_ax2 and found_ax11 and found_ax22):
+        if ax1==0 or ax2==len(xaxis) or ax11==0 or ax22==len(xaxis):
             error_message = 'Unable to locate the bandwidth edges'
-            raise PulseFuncException(error_message, 1)        
+            raise PulseFuncException(error_message, 1)
 
         if (ax1 == ax2) or (ax11 == ax22):
             error_message = 'Invalid Bandwidth edges: They are the same point'
-            raise PulseFuncException(error_message, 1) 
+            raise PulseFuncException(error_message, 1)
+
 
         # FIRST, ENSURE THAT REFOCUS AXIS IS Y - Find angle at zero frequency
         indx = int(ax1 + math.ceil((ax2-ax1)/2.0))
@@ -286,36 +279,25 @@ class RfResults(object):
         # INITIAL REFOCUS APPROXIMATION
         # - Use fft to get no of cycles (fre)
         # - First zero-pad the profile (rpro)
-        rpro = yprofile[ax1:ax2+1]
 
-        nrpro = np.hstack( [ np.zeros(len(rpro)), rpro, np.zeros(len(rpro))] ) 
-
-        fre0 = np.fft.fft(nrpro)
-        length = len(fre0)
-        fre =  np.append(fre0[int(math.ceil(length/2)):], fre0[:int(math.ceil(length/2))])    # this is a roll?
+        nrpro = np.pad(yprofile[ax1:ax2+1], [ax2+1-ax1, ax2+1-ax1])
+        fre = np.fft.fftshift(np.fft.fft(nrpro))
 
         # If two values in fre are the same in magnitude matlab actually looks 
         # at the phase and finds the one with the biggest phase, which we're 
         # not doing here. However, since we are using the first value in the
         # array that matches maxfre, fre_pos = np.nonzero(...), the phase thing 
         # is not and issue - according to Jerry.
-        absfre = np.abs(fre)
-        maxfre = np.max(absfre)
-        fre_pos = np.nonzero(absfre==maxfre)
 
-        # Since fre_pos is an array of arrays, (most likely with only one 
-        # value), this pulls out the first value. 
-        fre_pos = fre_pos[0][0]
+        fre_pos = np.argmax(np.abs(fre))
 
-        # The +1" is added at the end to purposely throw off the fit so that 
+        # The +1" is added at the end to purposely throw off the fit so that
         # the iterations (below) give a good curve for getting the maximum area.
         fre_n = (1.0/3.0)*(fre_pos - len(nrpro)/2 +1)    
 
-        # Get rotation factor
+        # Get rotation factor, Apply refocus to profile (yprofile)
         rotf = -fre_n/(pulse_bw_khz*pulse_dur_ms)
-
-        # Apply refocus to profile (yprofile)
-        npro = np.exp(1j*2*math.pi*rotf*xaxis*pulse_dur_ms)*yprofile
+        npro = np.exp(1j*2*np.pi*rotf*xaxis*pulse_dur_ms) * yprofile
 
         # Get initial area under real component, over twice the bandwidth
         # Added the next line so the following one would make sense.
@@ -323,30 +305,28 @@ class RfResults(object):
         aarea[0] = np.sum(np.real(npro[ax11-1:ax22]))
 
         # ITERATIVE FINE RE-PHASING ******************************
-        # - Now select just half the bandwidth
-        lnhnax = int(math.floor(len(rpro)/4.0))
+        # - select just half the bandwidth of imag data
+
+        lnhnax = int(math.floor((ax2+1-ax1) / 4.0))
         hnpro = np.imag(npro[ax1+lnhnax-1:ax2-lnhnax]) 
         hnax = xaxis[ax1+lnhnax-1:ax2-lnhnax]
-
-        p = np.polyfit(hnax,hnpro,1)
-        # Slope of line is p[0], intercept is p[1]
+        p = np.polyfit(hnax,hnpro,1)    # Slope of line is p[0], intercept is p[1]
 
         nrotf = np.zeros((40))
         barea = np.zeros((40))
-
         mtheta = math.atan(p[0])
 
-        nrotf[0] = -mtheta/(2.0*math.pi)
-        nnpro =  np.exp(1j*2.0*math.pi*nrotf[0]*xaxis*pulse_dur_ms/8.0) * npro
+        nrotf[0] = -mtheta/(2.0*np.pi)
+        nnpro =  np.exp(1j*2.0*np.pi*nrotf[0]*xaxis*pulse_dur_ms/8.0) * npro
         barea[0] = np.sum(np.real(nnpro[ax11-1:ax22]))
 
         niter = 1
-        while abs(p[0]) > .001 and niter < 19:
+        while abs(p[0]) > 0.001 and niter < 19:
             hnpro = np.imag(nnpro[ax1+lnhnax-1:ax2-lnhnax]) 
             p = np.polyfit(hnax,hnpro,1)
             mtheta = math.atan(p[0])
-            nrotf[niter] = -mtheta/(2.0*math.pi)
-            nnpro =  np.exp(1j*2.0*math.pi*nrotf[niter]*xaxis*pulse_dur_ms/8.0)*nnpro
+            nrotf[niter] = -mtheta/(2.0*np.pi)
+            nnpro =  np.exp(1j*2.0*np.pi*nrotf[niter]*xaxis*pulse_dur_ms/8.0)*nnpro
             barea[niter] = np.sum(np.real(nnpro[ax11-1:ax22]))
             niter = niter + 1
 
@@ -362,7 +342,7 @@ class RfResults(object):
         nrotf = np.append(nrotf, nrotf[::-1] )
 
         for niter in range(niter,(2*niter)):
-            nnpro =  np.exp(1j*2.0*math.pi*nrotf[niter]*xaxis*pulse_dur_ms/8.0)*nnpro
+            nnpro =  np.exp(1j*2.0*np.pi*nrotf[niter]*xaxis*pulse_dur_ms/8.0)*nnpro
             barea[niter] = np.sum(np.real(nnpro[ax11-1:ax22]))
 
         barea = np.append(aarea, barea[0:niter])
@@ -385,7 +365,176 @@ class RfResults(object):
             raise PulseFuncException(error_message, 1)           
 
         return frotn
-        
+
+
+    def grad_refocus_orig(self, xaxis0, yprofile0, pulse_dur_ms):
+        """
+        For single band pulses: Calculates the optimal
+        refocusing gradient as a fractional real number.
+
+        xaxis0 - The "x-axis" points of the profile - as an array (units of kHz)
+        yprofile0 - The "y-axis" points of the profile (units of fractional magnetization -1 to 1)
+        pulse_dur_ms - The pulse length in milliseconds.
+        pulse_bw_khz - The pulse bandwidth in khz.
+
+        """
+        # Get the bandwidth from the profile
+        pulse_bw_khz = self.get_bandwidth(xaxis0, yprofile0)
+        if pulse_bw_khz < 0:
+            error_message = 'Calculated bandwidth was less than zero'
+            raise PulseFuncException(error_message, 1)
+
+        # Get axis range over just bandwidth
+        xaxis = xaxis0.copy()
+        yprofile = yprofile0.copy()
+        bw_half = pulse_bw_khz/2.0
+
+        found_ax1 = False
+        found_ax2 = False
+        index = 0
+        for ii in xaxis:
+            if ii > -bw_half and not found_ax1:
+                # Want the first one, so if find one, don't reassign.
+                ax1 = index
+                found_ax1 = True
+            if ii < bw_half:
+                # Keep reassigning, since want last one.
+                ax2 = index
+                found_ax2 = True
+            index += 1
+
+        found_ax11 = False
+        found_ax22 = False
+        index = 0
+        for ii in xaxis:
+            if ii > -2*bw_half and not found_ax11:
+                # Want the first one, so if find one, don't reassign.
+                ax11 = index
+                found_ax11 = True
+            if ii < 2*bw_half:
+                # Keep reassigning, since want last one.
+                ax22 = index
+                found_ax22 = True
+            index += 1
+
+        # If not (found_ax1 and found_ax2), throw an error.
+        if not (found_ax1 and found_ax2 and found_ax11 and found_ax22):
+            error_message = 'Unable to locate the bandwidth edges'
+            raise PulseFuncException(error_message, 1)
+
+        if (ax1 == ax2) or (ax11 == ax22):
+            error_message = 'Invalid Bandwidth edges: They are the same point'
+            raise PulseFuncException(error_message, 1)
+
+        # FIRST, ENSURE THAT REFOCUS AXIS IS Y - Find angle at zero frequency
+        indx = int(ax1 + math.ceil((ax2-ax1)/2.0))
+        rot = np.angle(yprofile[indx])
+
+        yprofile = np.exp(-1j*rot)*yprofile
+
+        # INITIAL REFOCUS APPROXIMATION
+        # - Use fft to get no of cycles (fre)
+        # - First zero-pad the profile (rpro)
+        rpro = yprofile[ax1:ax2+1]
+
+        nrpro = np.hstack( [ np.zeros(len(rpro)), rpro, np.zeros(len(rpro))] )
+
+        fre0 = np.fft.fft(nrpro)
+        length = len(fre0)
+        fre =  np.append(fre0[int(math.ceil(length/2)):], fre0[:int(math.ceil(length/2))])    # this is a roll?
+
+        # If two values in fre are the same in magnitude matlab actually looks
+        # at the phase and finds the one with the biggest phase, which we're
+        # not doing here. However, since we are using the first value in the
+        # array that matches maxfre, fre_pos = np.nonzero(...), the phase thing
+        # is not and issue - according to Jerry.
+        absfre = np.abs(fre)
+        maxfre = np.max(absfre)
+        fre_pos = np.nonzero(absfre==maxfre)
+
+        # Since fre_pos is an array of arrays, (most likely with only one
+        # value), this pulls out the first value.
+        fre_pos = fre_pos[0][0]
+
+        # The +1" is added at the end to purposely throw off the fit so that
+        # the iterations (below) give a good curve for getting the maximum area.
+        fre_n = (1.0/3.0)*(fre_pos - len(nrpro)/2 +1)
+
+        # Get rotation factor
+        rotf = -fre_n/(pulse_bw_khz*pulse_dur_ms)
+
+        # Apply refocus to profile (yprofile)
+        npro = np.exp(1j*2*math.pi*rotf*xaxis*pulse_dur_ms)*yprofile
+
+        # Get initial area under real component, over twice the bandwidth
+        # Added the next line so the following one would make sense.
+        aarea = np.zeros(1)
+        aarea[0] = np.sum(np.real(npro[ax11-1:ax22]))
+
+        # ITERATIVE FINE RE-PHASING ******************************
+        # - Now select just half the bandwidth
+        lnhnax = int(math.floor(len(rpro)/4.0))
+        hnpro = np.imag(npro[ax1+lnhnax-1:ax2-lnhnax])
+        hnax = xaxis[ax1+lnhnax-1:ax2-lnhnax]
+
+        p = np.polyfit(hnax,hnpro,1)
+        # Slope of line is p[0], intercept is p[1]
+
+        nrotf = np.zeros((40))
+        barea = np.zeros((40))
+
+        mtheta = math.atan(p[0])
+
+        nrotf[0] = -mtheta/(2.0*math.pi)
+        nnpro =  np.exp(1j*2.0*math.pi*nrotf[0]*xaxis*pulse_dur_ms/8.0) * npro
+        barea[0] = np.sum(np.real(nnpro[ax11-1:ax22]))
+
+        niter = 1
+        while abs(p[0]) > .001 and niter < 19:
+            hnpro = np.imag(nnpro[ax1+lnhnax-1:ax2-lnhnax])
+            p = np.polyfit(hnax,hnpro,1)
+            mtheta = math.atan(p[0])
+            nrotf[niter] = -mtheta/(2.0*math.pi)
+            nnpro =  np.exp(1j*2.0*math.pi*nrotf[niter]*xaxis*pulse_dur_ms/8.0)*nnpro
+            barea[niter] = np.sum(np.real(nnpro[ax11-1:ax22]))
+            niter = niter + 1
+
+        # See if convergence failed.
+        if niter >= 19:
+            error_message = 'Convergence Failed'
+            raise PulseFuncException(error_message, 1)
+
+        nrotf = nrotf[0:niter]
+        refocus_value = (rotf + np.sum(nrotf)/8.0)
+
+        # Continue rotations to symetrize data
+        nrotf = np.append(nrotf, nrotf[::-1] )
+
+        for niter in range(niter,(2*niter)):
+            nnpro =  np.exp(1j*2.0*math.pi*nrotf[niter]*xaxis*pulse_dur_ms/8.0)*nnpro
+            barea[niter] = np.sum(np.real(nnpro[ax11-1:ax22]))
+
+        barea = np.append(aarea, barea[0:niter])
+        carea =  1000.0/barea
+        nrotf = nrotf[0:niter]
+        frot = rotf + np.cumsum(nrotf/8)
+        frot = np.append(rotf, frot)
+
+        # FINE TUNE FOR MAX AREA UNDER REAL PROFILE
+        pa = np.polyfit(frot,carea,2)
+        y = np.polyval(pa,frot)
+
+        # This is the value considering areas
+        frotn = -pa[1]/(2.0*pa[0])
+
+        # If p[0] > .001, or p[1]>.001, or niter > 19, iterations failed?
+
+        if frotn < 0:
+            error_message = 'Invalid Result: Fraction less than zero'
+            raise PulseFuncException(error_message, 1)
+
+        return frotn
+
 
     def update_profiles(self, outputs):  
         """
